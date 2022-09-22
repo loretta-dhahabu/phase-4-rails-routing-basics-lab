36d79696cb2bc4bcba0ed9ab5b32f0a58ef50c70
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-22 20:21:52 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #GET request to /students
+  get 'students', to: 'students#index'
+
+  #GET request to /students/grades
+  get 'students/grades', to: 'students#grades'
+
+  #GET request to /students/highest-grade
+  get 'students/highest-grade', to: 'students#highest_grade'
 end
